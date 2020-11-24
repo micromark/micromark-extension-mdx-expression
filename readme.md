5d74339b9832acdf63b53297b79ae6b7c71c16ad
Titus Wormer <tituswormer@gmail.com>
2020-11-24 15:27:01 +0100

    .

---
+++ readme.md
@@ -0,0 +1,264 @@
+# micromark-extension-mdx-expression
+
+[![Build][build-badge]][build]
+[![Coverage][coverage-badge]][coverage]
+[![Downloads][downloads-badge]][downloads]
+[![Size][size-badge]][size]
+[![Sponsors][sponsors-badge]][collective]
+[![Backers][backers-badge]][collective]
+[![Chat][chat-badge]][chat]
+
+**[micromark][]** extension to support [MDX][] or MDX JS expressions.
+
+This package provides the low-level modules for integrating with the micromark
+tokenizer but has no handling of compiling to HTML: go to a syntax tree instead.
+
+You should use this with `mdast-util-mdx-expression` (**[mdast][]**).
+Alternatively, use `micromark-extension-mdx` with `mdast-util-mdx` or
+`micromark-extension-mdxjs` with `mdast-util-mdxjs` to support all of MDX (JS).
+Or, use it through `remark-mdx` or `remark-mdxjs` (**[remark][]**).
+
+## Install
+
+[npm][]:
+
+```sh
+npm install micromark-extension-mdx-expression
+```
+
+## Use
+
+See `mdast-util-mdx-expression` for an example.
+
+## API
+
+### `syntax(options?)`
+
+Support [MDX][] (JS) expressions.
+
+The export of `syntax` is a function that can be called with options and returns
+an extension for the micromark parser (to tokenize expressions; can be passed in
+`extensions`).
+
+##### `options`
+
+###### `options.acorn`
+
+Acorn parser to use ([`Acorn`][acorn], optional).
+
+###### `options.acornOptions`
+
+Options to pass to acorn (`Object`, default: `{ecmaVersion: 2020, locations:
+true, sourceType: 'module'}`).
+All fields except for `locations` can be set.
+
+###### `options.addResult`
+
+Whether to add an `estree` field to the `mdxFlowExpression` and
+`mdxTextExpression` tokens with the results from acorn (`boolean`, default:
+`false`).
+Note that expressions can be empty or be just comments, in which cases `estree`
+will be undefined.
+
+## Syntax
+
+This extensions support both vanilla MDX and MDX JS.
+The first is agnostic to the programming language (it could contain Rust or
+so), the last is specific to JavaScript.
+To turn on gnostic mode, pass `acorn`.
+
+There are two types of expressions: in text (inline, span) or in flow (block).
+They start with `{`.
+
+Depending on whether `acorn` is passed, expressions are either parsed in several
+tries until whole JavaScript is found (as in, nested curly braces depend on JS
+expression nesting), or they are counted and must be balanced.
+
+Expressions end with `}`.
+
+For flow (block) expressions, optionally markdown spaces (` ` or `\t`) can occur
+after the closing brace, and finally a markdown line ending (`\r`, `\n`) or the
+end of the file must follow.
+
+While markdown typically knows no errors, for MDX it is decided to instead
+throw on invalid syntax.
+
+```markdown
+Here is an expression in a heading:
+
+## Hello, {1 + 1}!
+
+In agnostic mode, balanced braces can occur: {a + {b} + c}.
+
+In gnostic mode, the value of the expression must be JavaScript, so
+this would fail: {!}.
+But, in gnostic mode, braces can be in comments, strings, or in other
+places: {1 /* { */ + 2}.
+
+The previous examples were text (inline, span) expressions, they can
+also be flow (block):
+
+{
+  1 + 1
+}
+
+This is incorrect, because there are further characters:
+
+{
+  1 + 1
+}!
+
+Blank lines cannot occur in text, because markdown has already split them in
+separate constructs, so this is incorrect: {1 +
+
+1}
+
+
+In flow, you can have blank lines:
+
+{
+  1 +
+
+  2
+}
+```
+
+## Errors
+
+### Unexpected end of file in expression, expected a corresponding closing brace for `{`
+
+This error occurs if a `{` was seen without a `}` (source:
+`micromark-extension-mdx-expression`, rule id: `unexpected-eof`).
+For example:
+
+```markdown
+a { b
+```
+
+### Could not parse expression with acorn: $error
+
+This error occurs if acorn crashes (source: `micromark-extension-mdx-expression`,
+rule id: `acorn`).
+For example:
+
+```markdown
+a {var b = "c"} d
+```
+
+### Unexpected content after expression, expected `}`
+
+This error occurs when there is more content after a JS expression (source:
+`micromark-extension-mdx-expression`, rule id: `unexpected-content`).
+For example:
+
+```markdown
+a {"b" "c"} d
+```
+
+## Tokens
+
+Two tokens are used, `mdxFlowExpression` and `mdxTextExpression`, to reflect
+flow and text expressions.
+
+They include:
+
+*   `lineEnding` for the markdown line endings `\r`, `\n`, and `\r\n`
+*   `mdxFlowExpressionMarker` and `mdxTextExpressionMarker` for the braces
+*   `whitespace` for markdown spaces and tabs in blank lines
+*   `mdxFlowExpressionChunk` and `mdxTextExpressionChunk` for chunks of
+    expression content
+
+## Related
+
+*   [`remarkjs/remark`][remark]
+    — markdown processor powered by plugins
+*   [`micromark/micromark`][micromark]
+    — the smallest commonmark-compliant markdown parser that exists
+*   `micromark/micromark-extension-mdx`
+    — micromark extension to support all of MDX
+*   `micromark/micromark-extension-mdxjs`
+    — micromark extension to support all of MDX JS
+*   `micromark/micromark-extension-mdx-jsx`
+    — micromark extension to support the JSX of MDX
+*   [`micromark/micromark-extension-mdxjs-esm`][mdxjs-esm]
+    — micromark extension to support the import/exports of MDX JS
+*   `micromark/micromark-extension-mdx-md`
+    — micromark extension to support the changes to markdown of MDX
+*   `syntax-tree/mdast-util-mdx`
+    — mdast utility to support all of MDX
+*   `syntax-tree/mdast-util-mdxjs`
+    — mdast utility to support all of MDX JS
+*   [`syntax-tree/mdast-util-from-markdown`][from-markdown]
+    — mdast parser using `micromark` to create mdast from markdown
+*   [`syntax-tree/mdast-util-to-markdown`][to-markdown]
+    — mdast serializer to create markdown from mdast
+
+## Contribute
+
+See [`contributing.md` in `micromark/.github`][contributing] for ways to get
+started.
+See [`support.md`][support] for ways to get help.
+
+This project has a [code of conduct][coc].
+By interacting with this repository, organization, or community you agree to
+abide by its terms.
+
+## License
+
+[MIT][license] © [Titus Wormer][author]
+
+<!-- Definitions -->
+
+[build-badge]: https://github.com/micromark/micromark-extension-mdx-expression/workflows/main/badge.svg
+
+[build]: https://github.com/micromark/micromark-extension-mdx-expression/actions
+
+[coverage-badge]: https://img.shields.io/codecov/c/github/micromark/micromark-extension-mdx-expression.svg
+
+[coverage]: https://codecov.io/github/micromark/micromark-extension-mdx-expression
+
+[downloads-badge]: https://img.shields.io/npm/dm/micromark-extension-mdx-expression.svg
+
+[downloads]: https://www.npmjs.com/package/micromark-extension-mdx-expression
+
+[size-badge]: https://img.shields.io/bundlephobia/minzip/micromark-extension-mdx-expression.svg
+
+[size]: https://bundlephobia.com/result?p=micromark-extension-mdx-expression
+
+[sponsors-badge]: https://opencollective.com/unified/sponsors/badge.svg
+
+[backers-badge]: https://opencollective.com/unified/backers/badge.svg
+
+[collective]: https://opencollective.com/unified
+
+[chat-badge]: https://img.shields.io/badge/chat-discussions-success.svg
+
+[chat]: https://github.com/micromark/unist/discussions
+
+[npm]: https://docs.npmjs.com/cli/install
+
+[license]: license
+
+[author]: https://wooorm.com
+
+[contributing]: https://github.com/micromark/.github/blob/HEAD/contributing.md
+
+[support]: https://github.com/micromark/.github/blob/HEAD/support.md
+
+[coc]: https://github.com/micromark/.github/blob/HEAD/code-of-conduct.md
+
+[micromark]: https://github.com/micromark/micromark
+
+[from-markdown]: https://github.com/syntax-tree/mdast-util-from-markdown
+
+[to-markdown]: https://github.com/syntax-tree/mdast-util-to-markdown
+
+[remark]: https://github.com/remarkjs/remark
+
+[mdast]: https://github.com/syntax-tree/mdast
+
+[mdx]: https://github.com/mdx-js/mdx
+
+[acorn]: https://github.com/acornjs/acorn
+
+[mdxjs-esm]: https://github.com/micromark/micromark-extension-mdxjs-esm
---
+++ test.js
@@ -0,0 +1,571 @@
+var acorn = require('acorn')
+var jsx = require('acorn-jsx')
+var test = require('tape')
+var micromark = require('micromark/lib')
+var syntax = require('.')
+
+var html = {
+  enter: {mdxFlowExpression: start, mdxTextExpression: start},
+  exit: {mdxFlowExpression: end, mdxTextExpression: end}
+}
+
+function start() {
+  this.buffer()
+}
+
+function end() {
+  this.resume()
+  this.setData('slurpOneLineEnding', true)
+}
+
+test('micromark-extension-mdx-expression', function (t) {
+  t.throws(
+    function () {
+      syntax({acorn: true})
+    },
+    /Expected a proper `acorn` instance passed in as `options\.acorn`/,
+    'should throw if `acorn` is passed but it has no `parse`'
+  )
+
+  t.throws(
+    function () {
+      syntax({addResult: true})
+    },
+    /Expected an `acorn` instance passed in as `options\.acorn`/,
+    'should throw if `addResult` is passed w/o `acorn`'
+  )
+
+  t.throws(
+    function () {
+      syntax({acornOptions: {}})
+    },
+    /Expected an `acorn` instance passed in as `options\.acorn`/,
+    'should throw if `acornOptions` is passed w/o `acorn`'
+  )
+
+  t.throws(
+    function () {
+      micromark('a {<b />} c', {extensions: [syntax({acorn: acorn})]})
+    },
+    /Could not parse expression with acorn: SyntaxError: Unexpected token/,
+    'should not support JSX by default'
+  )
+
+  t.equal(
+    micromark('a {<b />} c', {
+      extensions: [syntax({acorn: acorn.Parser.extend(jsx())})],
+      htmlExtensions: [html]
+    }),
+    '<p>a  c</p>',
+    'should support JSX if an `acorn` instance supporting it is passed in'
+  )
+
+  t.throws(
+    function () {
+      micromark('a {(() => {})()} c', {
+        extensions: [syntax({acorn: acorn, acornOptions: {ecmaVersion: 5}})]
+      })
+    },
+    /Could not parse expression with acorn: SyntaxError: Unexpected token/,
+    'should support `acornOptions` (1)'
+  )
+
+  t.equal(
+    micromark('a {(() => {})()} c', {
+      extensions: [syntax({acorn: acorn, acornOptions: {ecmaVersion: 6}})],
+      htmlExtensions: [html]
+    }),
+    '<p>a  c</p>',
+    'should support `acornOptions` (2)'
+  )
+
+  t.equal(
+    micromark('a {b} c', {
+      extensions: [syntax({acorn: acorn, addResult: true})],
+      htmlExtensions: [
+        {
+          enter: {
+            mdxFlowExpression: checkResultExpression,
+            mdxTextExpression: checkResultExpression
+          },
+          exit: {mdxFlowExpression: end, mdxTextExpression: end}
+        }
+      ]
+    }),
+    '<p>a  c</p>',
+    'should support `addResult`'
+  )
+
+  function checkResultExpression(token) {
+    t.ok(
+      'estree' in token,
+      '`addResult` should add `estree` to expression tokens'
+    )
+    t.equal(
+      token.estree.type,
+      'Identifier',
+      '`addResult` should add an expression'
+    )
+    return start.call(this, token)
+  }
+
+  t.equal(
+    micromark('a {} c', {
+      extensions: [syntax({acorn: acorn, addResult: true})],
+      htmlExtensions: [
+        {
+          enter: {
+            mdxFlowExpression: checkResultEmpty,
+            mdxTextExpression: checkResultEmpty
+          },
+          exit: {mdxFlowExpression: end, mdxTextExpression: end}
+        }
+      ]
+    }),
+    '<p>a  c</p>',
+    'should support `addResult` for an empty expression'
+  )
+
+  function checkResultEmpty(token) {
+    t.ok(
+      'estree' in token,
+      '`addResult` should add `estree` to expression tokens'
+    )
+    t.equal(
+      token.estree,
+      undefined,
+      '`estree` should be empty for an empty expression'
+    )
+    return start.call(this, token)
+  }
+
+  t.equal(
+    micromark('a {} b', {
+      extensions: [syntax({acorn: acorn})],
+      htmlExtensions: [html]
+    }),
+    '<p>a  b</p>',
+    'should support an empty expression (1)'
+  )
+
+  t.equal(
+    micromark('a { \t\r\n} b', {
+      extensions: [syntax({acorn: acorn})],
+      htmlExtensions: [html]
+    }),
+    '<p>a  b</p>',
+    'should support an empty expression (2)'
+  )
+
+  t.equal(
+    micromark('a {/**/} b', {
+      extensions: [syntax({acorn: acorn})],
+      htmlExtensions: [html]
+    }),
+    '<p>a  b</p>',
+    'should support a multiline comment (1)'
+  )
+
+  t.equal(
+    micromark('a {  /*\n*/\t} b', {
+      extensions: [syntax({acorn: acorn})],
+      htmlExtensions: [html]
+    }),
+    '<p>a  b</p>',
+    'should support a multiline comment (2)'
+  )
+
+  t.equal(
+    micromark('a {/*b*//*c*/} d', {
+      extensions: [syntax({acorn: acorn})],
+      htmlExtensions: [html]
+    }),
+    '<p>a  d</p>',
+    'should support a multiline comment (3)'
+  )
+
+  t.equal(
+    micromark('a {b/*c*/} d', {
+      extensions: [syntax({acorn: acorn})],
+      htmlExtensions: [html]
+    }),
+    '<p>a  d</p>',
+    'should support a multiline comment (4)'
+  )
+
+  t.equal(
+    micromark('a {/*b*/c} d', {
+      extensions: [syntax({acorn: acorn})],
+      htmlExtensions: [html]
+    }),
+    '<p>a  d</p>',
+    'should support a multiline comment (4)'
+  )
+
+  t.throws(
+    function () {
+      micromark('a {//} b', {extensions: [syntax({acorn: acorn})]})
+    },
+    /Could not parse expression with acorn: SyntaxError: Unexpected token/,
+    'should crash on a line comment (1)'
+  )
+
+  t.throws(
+    function () {
+      micromark('a { // b } c', {extensions: [syntax({acorn: acorn})]})
+    },
+    /Could not parse expression with acorn: SyntaxError: Unexpected token/,
+    'should crash on a line comment (2)'
+  )
+
+  t.equal(
+    micromark('a {//\n} b', {
+      extensions: [syntax({acorn: acorn})],
+      htmlExtensions: [html]
+    }),
+    '<p>a  b</p>',
+    'should support a line comment followed by a line ending'
+  )
+
+  t.equal(
+    micromark('a {// b\nd} d', {
+      extensions: [syntax({acorn: acorn})],
+      htmlExtensions: [html]
+    }),
+    '<p>a  d</p>',
+    'should support a line comment followed by a line ending and an expression'
+  )
+
+  t.equal(
+    micromark('a {b// c\n} d', {
+      extensions: [syntax({acorn: acorn})],
+      htmlExtensions: [html]
+    }),
+    '<p>a  d</p>',
+    'should support an expression followed by a line comment and a line ending'
+  )
+
+  t.equal(
+    micromark('a {b.c} d', {
+      extensions: [syntax({acorn: acorn})],
+      htmlExtensions: [html]
+    }),
+    '<p>a  d</p>',
+    'should support expression statements (1)'
+  )
+
+  t.equal(
+    micromark('a {1 + 1} b', {
+      extensions: [syntax({acorn: acorn})],
+      htmlExtensions: [html]
+    }),
+    '<p>a  b</p>',
+    'should support expression statements (2)'
+  )
+
+  t.equal(
+    micromark('a {function () {}} b', {
+      extensions: [syntax({acorn: acorn})],
+      htmlExtensions: [html]
+    }),
+    '<p>a  b</p>',
+    'should support expression statements (3)'
+  )
+
+  t.throws(
+    function () {
+      micromark('a {var b = "c"} d', {extensions: [syntax({acorn: acorn})]})
+    },
+    /Could not parse expression with acorn: SyntaxError: Unexpected token/,
+    'should crash on non-expressions'
+  )
+
+  t.equal(
+    micromark('> a {\n> b} c', {
+      extensions: [syntax({acorn: acorn})],
+      htmlExtensions: [html]
+    }),
+    '<blockquote>\n<p>a  c</p>\n</blockquote>',
+    'should support expressions in containers'
+  )
+
+  t.throws(
+    function () {
+      micromark('> a {\n> b<} c', {extensions: [syntax({acorn: acorn})]})
+    },
+    /Could not parse expression with acorn: SyntaxError: Unexpected token/,
+    'should crash on incorrect expressions in containers (1)'
+  )
+
+  t.throws(
+    function () {
+      micromark('> a {\n> b\n> c} d', {extensions: [syntax({acorn: acorn})]})
+    },
+    /Unexpected content after expression, expected `}`/,
+    'should crash on incorrect expressions in containers (2)'
+  )
+
+  test('text (agnostic)', function (t) {
+    t.equal(
+      micromark('a {b} c', {extensions: [syntax()], htmlExtensions: [html]}),
+      '<p>a  c</p>',
+      'should support an expression'
+    )
+
+    t.equal(
+      micromark('a {} b', {extensions: [syntax()], htmlExtensions: [html]}),
+      '<p>a  b</p>',
+      'should support an empty expression'
+    )
+
+    t.throws(
+      function () {
+        micromark('a {b c', {extensions: [syntax()]})
+      },
+      /Unexpected end of file in expression, expected a corresponding closing brace for `{`/,
+      'should crash if no closing brace is found (1)'
+    )
+
+    t.throws(
+      function () {
+        micromark('a {b { c } d', {extensions: [syntax()]})
+      },
+      /Unexpected end of file in expression, expected a corresponding closing brace for `{`/,
+      'should crash if no closing brace is found (2)'
+    )
+
+    t.equal(
+      micromark('a {\n} b', {extensions: [syntax()], htmlExtensions: [html]}),
+      '<p>a  b</p>',
+      'should support a line ending in an expression'
+    )
+
+    t.equal(
+      micromark('a } b', {extensions: [syntax()], htmlExtensions: [html]}),
+      '<p>a } b</p>',
+      'should support just a closing brace'
+    )
+
+    t.equal(
+      micromark('{ a } b', {extensions: [syntax()], htmlExtensions: [html]}),
+      '<p> b</p>',
+      'should support expressions as the first thing when following by other things'
+    )
+
+    t.end()
+  })
+
+  test('text (gnostic)', function (t) {
+    t.equal(
+      micromark('a {b} c', {
+        extensions: [syntax({acorn: acorn})],
+        htmlExtensions: [html]
+      }),
+      '<p>a  c</p>',
+      'should support an expression'
+    )
+
+    t.throws(
+      function () {
+        micromark('a {??} b', {extensions: [syntax({acorn: acorn})]})
+      },
+      /Could not parse expression with acorn: SyntaxError: Unexpected token/,
+      'should crash on an incorrect expression'
+    )
+
+    t.equal(
+      micromark('a {} b', {
+        extensions: [syntax({acorn: acorn})],
+        htmlExtensions: [html]
+      }),
+      '<p>a  b</p>',
+      'should support an empty expression'
+    )
+
+    t.throws(
+      function () {
+        micromark('a {b c', {extensions: [syntax({acorn: acorn})]})
+      },
+      /Unexpected end of file in expression, expected a corresponding closing brace for `{`/,
+      'should crash if no closing brace is found (1)'
+    )
+
+    t.throws(
+      function () {
+        micromark('a {b { c } d', {extensions: [syntax({acorn: acorn})]})
+      },
+      /Unexpected content after expression, expected `}`/,
+      'should crash if no closing brace is found (2)'
+    )
+
+    t.equal(
+      micromark('a {\n} b', {
+        extensions: [syntax({acorn: acorn})],
+        htmlExtensions: [html]
+      }),
+      '<p>a  b</p>',
+      'should support a line ending in an expression'
+    )
+
+    t.equal(
+      micromark('a } b', {
+        extensions: [syntax({acorn: acorn})],
+        htmlExtensions: [html]
+      }),
+      '<p>a } b</p>',
+      'should support just a closing brace'
+    )
+
+    t.equal(
+      micromark('{ a } b', {
+        extensions: [syntax({acorn: acorn})],
+        htmlExtensions: [html]
+      }),
+      '<p> b</p>',
+      'should support expressions as the first thing when following by other things'
+    )
+
+    t.equal(
+      micromark('a { /* { */ } b', {
+        extensions: [syntax({acorn: acorn})],
+        htmlExtensions: [html]
+      }),
+      '<p>a  b</p>',
+      'should support an unbalanced opening brace (if JS permits)'
+    )
+
+    t.equal(
+      micromark('a { /* } */ } b', {
+        extensions: [syntax({acorn: acorn})],
+        htmlExtensions: [html]
+      }),
+      '<p>a  b</p>',
+      'should support an unbalanced closing brace (if JS permits)'
+    )
+
+    t.end()
+  })
+
+  test('flow (agnostic)', function (t) {
+    t.equal(
+      micromark('{a}', {extensions: [syntax()], htmlExtensions: [html]}),
+      '',
+      'should support an expression'
+    )
+
+    t.equal(
+      micromark('{}', {extensions: [syntax()], htmlExtensions: [html]}),
+      '',
+      'should support an empty expression'
+    )
+
+    t.throws(
+      function () {
+        micromark('{a', {extensions: [syntax()]})
+      },
+      /Unexpected end of file in expression, expected a corresponding closing brace for `{`/,
+      'should crash if no closing brace is found (1)'
+    )
+
+    t.throws(
+      function () {
+        micromark('{b { c }', {extensions: [syntax()]})
+      },
+      /Unexpected end of file in expression, expected a corresponding closing brace for `{`/,
+      'should crash if no closing brace is found (2)'
+    )
+
+    t.equal(
+      micromark('{\n}\na', {extensions: [syntax()], htmlExtensions: [html]}),
+      '<p>a</p>',
+      'should support a line ending in an expression'
+    )
+
+    t.equal(
+      micromark('{ a } \t\nb', {
+        extensions: [syntax()],
+        htmlExtensions: [html]
+      }),
+      '<p>b</p>',
+      'should support expressions followed by spaces'
+    )
+
+    t.equal(
+      micromark('  { a }\nb', {
+        extensions: [syntax()],
+        htmlExtensions: [html]
+      }),
+      '<p>b</p>',
+      'should support expressions preceded by spaces'
+    )
+
+    t.end()
+  })
+
+  test('flow (gnostic)', function (t) {
+    t.equal(
+      micromark('{a}', {
+        extensions: [syntax({acorn: acorn})],
+        htmlExtensions: [html]
+      }),
+      '',
+      'should support an expression'
+    )
+
+    t.equal(
+      micromark('{}', {
+        extensions: [syntax({acorn: acorn})],
+        htmlExtensions: [html]
+      }),
+      '',
+      'should support an empty expression'
+    )
+
+    t.throws(
+      function () {
+        micromark('{a', {extensions: [syntax({acorn: acorn})]})
+      },
+      /Unexpected end of file in expression, expected a corresponding closing brace for `{`/,
+      'should crash if no closing brace is found (1)'
+    )
+
+    t.throws(
+      function () {
+        micromark('{b { c }', {extensions: [syntax({acorn: acorn})]})
+      },
+      /Unexpected content after expression, expected `}`/,
+      'should crash if no closing brace is found (2)'
+    )
+
+    t.equal(
+      micromark('{\n}\na', {
+        extensions: [syntax({acorn: acorn})],
+        htmlExtensions: [html]
+      }),
+      '<p>a</p>',
+      'should support a line ending in an expression'
+    )
+
+    t.equal(
+      micromark('{ a } \t\nb', {
+        extensions: [syntax({acorn: acorn})],
+        htmlExtensions: [html]
+      }),
+      '<p>b</p>',
+      'should support expressions followed by spaces'
+    )
+
+    t.equal(
+      micromark('  { a }\nb', {
+        extensions: [syntax({acorn: acorn})],
+        htmlExtensions: [html]
+      }),
+      '<p>b</p>',
+      'should support expressions preceded by spaces'
+    )
+
+    t.end()
+  })
+
+  t.end()
+})
